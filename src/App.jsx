747a200afcf818e7644a3992969e00a47e5bab59
Ehrvayn <erolivera@gbox.ncf.edu.ph>
2024-11-25 19:16:05 +0800

    part-5

--- src/App.jsx
+++ src/App.jsx
@@ -102,18 +102,22 @@ function App() {
             <div className="education-content">
               <div className="blur-box show">
                 <div className="education-box">
+                <img src="stacruz.jpg" alt="elementary" className="education-image" />
                   <div className="education-box-title">Elementary</div>
                   <div className="education-box-content">Sta Cruz Elementary School <br/> 2011-2017</div>
                 </div>
                 <div className="education-box">
+                <img src="camhigh.jpg" alt="elementary" className="education-image" />
                   <div className="education-box-title">Junior High School</div>
                   <div className="education-box-content">Camarines Sur National Highschool <br/>Basic Education Program (B.E.P)<br/> 2017-2021</div>
                 </div>
                 <div className="education-box">
+                <img src="camhigh.jpg" alt="elementary" className="education-image" />
                   <div className="education-box-title">Senior High School</div>
                   <div className="education-box-content">Camarines Sur National Highschool <br/>Technical-Vocational-Livelihood (T.V.L) <br/>2021-2023</div>
                 </div>
                 <div className="education-box">
+                <img src="ncf.jpg" alt="elementary" className="education-image" />
                   <div className="education-box-title">College</div>
                   <div className="education-box-content">Naga College Foundation Inc. <br/> Bachelor of Science in Information System (BSIS)</div>
                 </div>
